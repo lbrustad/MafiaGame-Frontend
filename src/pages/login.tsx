2386e238b9584a49612dea50c13d15b0e60223f0
Lasse Brustad <lasse.brustad@gmail.com>
2020-04-27 20:43:32 +0200

    Minor changes
    The UI is dynamic now, starting with the login only - login based on localhost works fine, will not work without localhost server

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -2,66 +2,146 @@ import React from "react";
 import { randomString } from "../util/string.util";
 
 export default class Login extends React.Component {
-  state = {
+  state: states = {
     username: "",
-    password: ""
+    password: "",
+    fetchData: {}
   };
 
+  elements = {
+    username: randomString(10),
+    password: randomString(10),
+    submit: randomString(10)
+  };
+
+  constructor(props: Readonly<{}>) {
+    super(props);
+
+    this.state = {
+      username: "",
+      password: "",
+      fetchData: null
+    };
+
+    this.changeUsername = this.changeUsername.bind(this);
+    this.changePassword = this.changePassword.bind(this);
+    this.submit = this.submit.bind(this);
+  }
+
+  changeUsername(e: React.ChangeEvent) {
+    const el = document.getElementById(this.elements.username);
+    if (el !== null && el instanceof HTMLInputElement) {
+      this.setState({ username: el.value });
+    }
+  }
+
+  changePassword = (e: React.ChangeEvent) => {
+    const el = document.getElementById(this.elements.password);
+    if (el !== null && el instanceof HTMLInputElement) {
+      this.setState({ password: el.value });
+    }
+  };
+
+  async submit(e: React.FormEvent) {
+    e.preventDefault();
+    const res = await fetch("http://localhost/api/login", {
+      method: "POST",
+      cache: "no-cache",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      referrerPolicy: "no-referrer",
+      body: JSON.stringify({
+        username: this.state.username,
+        password: this.state.password
+      })
+    });
+
+    const data: fetchResponse = await res.json();
+
+    console.log(data);
+    this.setState({ fetchData: data });
+
+    // remove message after 5 sec
+    setTimeout(() => {
+      this.setState({ fetchData: null })
+    }, 5e3)
+  }
+
   render() {
-    const elements = {
-      username: randomString(10),
-      password: randomString(10)
-    };
-    let passwd;
-
-    const changeUsername = (e: React.ChangeEvent) => {
-      const el = document.getElementById(elements.username);
-      if (el !== null && el instanceof HTMLInputElement) {
-        this.setState({ username: el.value });
-      }
-    };
-
-    const changePassword = (e: React.ChangeEvent) => {
-      const el = document.getElementById(elements.password);
-      if (el !== null && el instanceof HTMLInputElement) {
-        this.setState({ password: el.value });
-      }
-    };
-
-    const submit = () => {
-      //
-      return false;
-    };
+    let passwd, submit, info;
 
     if (
       typeof this.state.username === "string" &&
-      this.state.username.length >= 6
+      this.state.username.length >= 2
     ) {
       passwd = (
-        <input
-          id={elements.password}
-          type="password"
-          placeholder="Passord"
-          defaultValue={this.state.password}
-          onChange={changePassword}
-        />
+        <span>
+          <br />
+          <input
+            id={this.elements.password}
+            type="password"
+            placeholder="Passord"
+            defaultValue={this.state.password}
+            onChange={this.changePassword}
+          />
+        </span>
+      );
+    }
+
+    if (
+      typeof this.state.password === "string" &&
+      this.state.password.length >= 6
+    ) {
+      submit = (
+        <span>
+          <br />
+          <input type="submit" value="Logg inn" />
+        </span>
+      );
+    }
+
+    if (typeof this.state.fetchData?.message === "string") {
+      info = <p className="error">{this.state.fetchData.message}</p>;
+    }
+
+    if (typeof this.state.fetchData?.id === "string") {
+      info = (
+        <p className="success">{"Laster ID: " + this.state.fetchData.id}</p>
       );
     }
 
     return (
       <div>
-        <p>Login Page</p>
-        <form onSubmit={submit}>
+        <h1>Innlogging</h1>
+        <form onSubmit={this.submit}>
           <input
-            id={elements.username}
+            id={this.elements.username}
             type="text"
             placeholder="Brukernavn"
             defaultValue={this.state.username}
-            onChange={changeUsername}
+            onChange={this.changeUsername}
           />
           {passwd}
+          {submit}
         </form>
+        {info}
       </div>
     );
   }
+}
+
+interface states {
+  username: string;
+  password: string;
+  fetchData?: fetchResponse | null;
+}
+
+interface fetchResponse {
+  status?: "success" | "failed";
+  // success
+  id?: string | null;
+  // failed
+  statusCode?: number;
+  message?: string | null;
 }
