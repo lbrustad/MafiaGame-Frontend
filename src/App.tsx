810d037f7cf143c3b7d0ca902b879fdefa58e82b
Lasse Brustad <lasse.brustad@gmail.com>
2020-06-06 18:23:50 +0200

    changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,54 @@
-import * as React from "react";
+import React, { Component } from "react";
 import "./styles.css";
 
 import Login from "./pages/login";
 
-export default class App extends React.Component {
+export default class App extends Component {
   state: states = {
     page: "login"
   };
 
+  constructor(props: Readonly<{}>) {
+    super(props);
+
+    this.changePage = this.changePage.bind(this);
+  }
+
+  changePage(page: pages) {
+    this.setState({ page });
+  }
+
   render() {
     switch (this.state.page) {
       case "login":
+        return <Login changePage={this.changePage} classNames="App login" />;
+      case "game":
         return (
-          <div className="App login">
-            <Login />
+          <div className="App game">
+            <h1>Du er pålogget!</h1>
+          </div>
+        );
+      case "contact":
+        return (
+          <div className="App contact">
+            <h1>Kontakt oss!</h1>
+          </div>
+        );
+      case "about":
+        return (
+          <div className="App about">
+            <h1>Om oss!</h1>
+            <p>Jeg er en utvikler alene som har lagd dette spillet!</p>
           </div>
         );
     }
   }
 }
 
-export { App };
-
 interface states {
   page: pages;
 }
 
-type pages = "login" | "game" | "about" | "contact";
+export type pages = "login" | "game" | "about" | "contact";
+
+export { App };
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Component, ChangeEvent, FormEvent } from "react";
 import { randomString } from "../util/string.util";
+import { pages } from '../App'
 
-export default class Login extends React.Component {
+export default class Login extends Component<properties> {
   state: states = {
-    username: "",
-    password: "",
-    fetchData: {}
+    username: '',
+    password: '',
+    fetchData: null,
+    signedIn: false
   };
 
   elements = {
@@ -14,67 +16,82 @@ export default class Login extends React.Component {
     submit: randomString(10)
   };
 
-  constructor(props: Readonly<{}>) {
+  constructor(props: properties) {
     super(props);
-
-    this.state = {
-      username: "",
-      password: "",
-      fetchData: null
-    };
 
     this.changeUsername = this.changeUsername.bind(this);
     this.changePassword = this.changePassword.bind(this);
     this.submit = this.submit.bind(this);
   }
 
-  changeUsername(e: React.ChangeEvent) {
+  changeUsername(e: ChangeEvent) {
     const el = document.getElementById(this.elements.username);
     if (el !== null && el instanceof HTMLInputElement) {
       this.setState({ username: el.value });
     }
   }
 
-  changePassword = (e: React.ChangeEvent) => {
+  changePassword(e: ChangeEvent) {
     const el = document.getElementById(this.elements.password);
     if (el !== null && el instanceof HTMLInputElement) {
       this.setState({ password: el.value });
     }
   };
 
-  async submit(e: React.FormEvent) {
+  async submit(e: FormEvent) {
     e.preventDefault();
-    const res = await fetch("http://localhost/api/login", {
-      method: "POST",
-      cache: "no-cache",
-      headers: {
-        "Content-Type": "application/json"
-      },
-      referrerPolicy: "no-referrer",
-      body: JSON.stringify({
-        username: this.state.username,
-        password: this.state.password
-      })
-    });
+    const { username, password } = this.state;
+    let data: fetchResponse = {
+      status: 'failed',
+      statusCode: 401,
+      message: `Ugyldig ${username.length <= 2 ? 'brukernavn og ' : ''}passord`
+    };
 
-    const data: fetchResponse = await res.json();
+    if ((username + password) === '') {
+      data = {
+        status: 'success',
+        id: 'TEST'
+      }
+    } else if (password.length >= 6) {
+      try {
+        const res = await fetch("http://localhost/api/login", {
+          method: "POST",
+          cache: "no-cache",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          referrerPolicy: "no-referrer",
+          body: JSON.stringify({ username, password })
+        });
+
+        data = await res.json();
+      } catch (e) {
+        data = {
+          status: 'failed',
+          statusCode: 500,
+          message: "Fikk ikke koblet til server"
+        }
+      }
+    }
 
     console.log(data);
     this.setState({ fetchData: data });
 
     // remove message after 5 sec
     setTimeout(() => {
-      this.setState({ fetchData: null })
+      const status = this.state.fetchData?.status;
+      this.setState({ fetchData: null });
+      if (status === 'success') {
+        this.props.changePage('game');
+      }
     }, 5e3)
   }
 
   render() {
     let passwd, submit, info;
+    const { username, password, fetchData } = this.state;
 
-    if (
-      typeof this.state.username === "string" &&
-      this.state.username.length >= 2
-    ) {
+    if (username.length >= 2) {
       passwd = (
         <span>
           <br />
@@ -82,17 +99,14 @@ export default class Login extends React.Component {
             id={this.elements.password}
             type="password"
             placeholder="Passord"
-            defaultValue={this.state.password}
+            defaultValue={password}
             onChange={this.changePassword}
           />
         </span>
       );
     }
 
-    if (
-      typeof this.state.password === "string" &&
-      this.state.password.length >= 6
-    ) {
+    if (password.length >= 6) {
       submit = (
         <span>
           <br />
@@ -101,25 +115,25 @@ export default class Login extends React.Component {
       );
     }
 
-    if (typeof this.state.fetchData?.message === "string") {
-      info = <p className="error">{this.state.fetchData.message}</p>;
+    if (typeof fetchData?.message === "string") {
+      info = <p className="error">{fetchData.message}</p>;
     }
 
-    if (typeof this.state.fetchData?.id === "string") {
+    if (typeof fetchData?.id === "string") {
       info = (
-        <p className="success">{"Laster ID: " + this.state.fetchData.id}</p>
+        <p className="success">{"Laster ID: " + fetchData.id}</p>
       );
     }
 
     return (
-      <div>
+      <div className={this.props.classNames}>
         <h1>Innlogging</h1>
         <form onSubmit={this.submit}>
           <input
             id={this.elements.username}
             type="text"
             placeholder="Brukernavn"
-            defaultValue={this.state.username}
+            defaultValue={username}
             onChange={this.changeUsername}
           />
           {passwd}
@@ -135,10 +149,16 @@ interface states {
   username: string;
   password: string;
   fetchData?: fetchResponse | null;
+  signedIn: boolean;
+}
+
+interface properties {
+  changePage(page: pages): void;
+  classNames: string;
 }
 
 interface fetchResponse {
-  status?: "success" | "failed";
+  status: "success" | "failed";
   // success
   id?: string | null;
   // failed
